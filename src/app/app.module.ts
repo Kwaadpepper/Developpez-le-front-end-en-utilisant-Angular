30e5741f971c6e5a828cb7eb20c00961e92c81ce
Jeremy Munsch <github@jeremydev.ovh>
2024-09-21 09:11:12 +0200

    feat: Make poc of new olympic pie chart usage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OlympicPieChartComponent } from "./components/olympic-pie-chart/olympic-pie-chart.component";
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, OlympicPieChartComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { cloneDeep } from 'lodash';
+import { Observable, of, Subscription } from 'rxjs';
+import OlympicCountry from 'src/app/core/models/OlympicCountry';
 import { OlympicService, OlympicServiceData } from 'src/app/core/services/olympic.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  public olympics$: Observable<OlympicServiceData> = of([]);
+export class HomeComponent implements OnInit, OnDestroy {
+  private olympics$: Observable<OlympicServiceData> = of([]);
+  private olympicsSub: Subscription|null = null;
+
+  public olympicCountries: Array<OlympicCountry> = [];
+
+  public pieChartOptions: {
+    scheme: string;
+    legend: boolean;
+    labels: boolean;
+  } = {
+      legend: false,
+      labels: true,
+      scheme: 'cool'
+  };
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    this.olympicsSub = this.olympics$.subscribe((olympicCountryArray) => {
+      this.olympicCountries = cloneDeep(olympicCountryArray);
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.olympicsSub?.unsubscribe();
   }
 }
