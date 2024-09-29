195da73b9bb5b0b02b024ca9dd5e00e0a880d2e8
Jérémy Munsch <github@jeremydev.ovh>
2024-09-29 09:50:52 +0200

    main.ts add shoelace note

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import { AppModule } from './app/app.module'
 import { environment } from './environments/environment'
 
 if (environment.production) {
+  // For shoelace using lit, see: https://github.com/lit/lit/discussions/3671#discussioncomment-4977000
   enableProdMode()
 }
 
