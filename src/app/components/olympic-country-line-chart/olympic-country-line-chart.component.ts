c5005455e0fd11d26f1d89222255722575be2328
Jérémy Munsch <github@jeremydev.ovh>
2024-10-02 19:04:18 +0200

    refactor: :lipstick: line chart Ux add color and tooltip customisation

--- src/app/components/olympic-country-line-chart/olympic-country-line-chart.component.ts
+++ src/app/components/olympic-country-line-chart/olympic-country-line-chart.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core'
-import { ChartComponent, NgApexchartsModule } from 'ng-apexcharts'
+import { ApexAxisChartSeries, ChartComponent, NgApexchartsModule } from 'ng-apexcharts'
 import Participation from 'src/app/core/models/participation.interface'
+import OlympicConfig from 'src/app/core/OlympicConfig'
+import { CustomTooltip } from 'src/app/core/types/apex-charts/apex-tooltip-custom.type'
 import { PickApexOptions } from 'src/app/core/types/apex-charts/pick-apex-options.type'
 
 @Component({
@@ -16,6 +18,7 @@ export class OlympicCountryLineChartComponent implements OnInit, OnDestroy {
   @Input({ required: true }) participations: Participation[] = []
 
   public chart: PickApexOptions<'chart'>
+  public colors: PickApexOptions<'colors'>
   public dataLabels: PickApexOptions<'dataLabels'>
   public grid: PickApexOptions<'grid'>
   public labels: PickApexOptions<'labels'>
@@ -53,6 +56,7 @@ export class OlympicCountryLineChartComponent implements OnInit, OnDestroy {
         },
       },
     }
+    this.colors = OlympicConfig.getColors()
     this.dataLabels = { enabled: false }
     this.grid = {
       xaxis: { lines: { show: true } },
@@ -62,7 +66,31 @@ export class OlympicCountryLineChartComponent implements OnInit, OnDestroy {
     this.legend = { show: false, floating: false }
     this.series = []
     this.stroke = { curve: 'straight' }
-    this.tooltip = { enabled: true, x: { format: 'yyyy' } }
+    this.tooltip = {
+      enabled: true,
+      x: { format: 'yyyy' },
+      custom: (
+        { series, seriesIndex, dataPointIndex, w }:
+          Omit<CustomTooltip, 'series'> & { series: ApexAxisChartSeries },
+      ): string => {
+        const serie = series[seriesIndex]
+        const dataString = serie instanceof Array ? serie[dataPointIndex] : 0
+        const dataLabel = $localize`${dataString} \u{1F3C5}\u{00A0} Medal${dataString ? 's' : ''} won `
+
+        const div = document.createElement('div')
+        div.classList.add('olympic-tooltip')
+        const spanCountry = document.createElement('span')
+        spanCountry.innerText = dataLabel
+        const spanMedals = document.createElement('span')
+        // U+1F3C5 = 🏅  U+00A0 = &nbsp;
+        spanMedals.innerText = `in ${w.globals.categoryLabels[dataPointIndex]}`
+
+        div.appendChild(spanCountry)
+        div.appendChild(spanMedals)
+
+        return div.outerHTML
+      },
+    }
     this.yaxis = {
       min: 0,
       max: (): number => {
@@ -98,7 +126,7 @@ export class OlympicCountryLineChartComponent implements OnInit, OnDestroy {
       .map((participation: Participation) => participation.year)
     this.series = [
       {
-        name: 'Medals this year',
+        name: $localize`Medals this year`,
         type: 'line',
         data: participations.map((participation: Participation) => participation.medalsCount),
       } as never,
--- src/app/core/types/apex-charts/apex-tooltip-custom.type.ts
+++ src/app/core/types/apex-charts/apex-tooltip-custom.type.ts
@@ -8,6 +8,7 @@ export interface CustomTooltip {
     config: ApexOptions
     globals: object & {
       labels: string[] | string[][]
+      categoryLabels: string[]
     }
   }
 }
