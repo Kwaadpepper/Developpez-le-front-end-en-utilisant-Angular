fcaaa95e3e824ca7d86d003e1d11fbd43b5c4297
Jérémy Munsch <github@jeremydev.ovh>
2024-10-04 13:50:10 +0200

    feat: :lipstick: Enhance 404 page style

--- src/app/pages/not-found/not-found.component.spec.ts
+++ src/app/pages/not-found/not-found.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
+import { AppComponent } from 'src/app/app.component'
 import { NotFoundComponent } from './not-found.component'
 
 describe('NotFoundComponent', () => {
@@ -8,7 +10,8 @@ describe('NotFoundComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [NotFoundComponent],
+      declarations: [AppComponent, NotFoundComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
       .compileComponents()
 
